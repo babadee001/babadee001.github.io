cf389ac498fee2d447b0c2a6413184cf4e5c44ee
babadee001 <temidayooyedele300@gmail.com>
2017-08-03 18:54:15 +0100

    add signup controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -11,4 +11,22 @@ module.exports = {
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
   },
+  retrieve(req, res) {
+  return User
+    .findByDetails(req.params.todoId, {
+      include: [{
+        model: TodoItem,
+        as: 'todoItems',
+      }],
+    })
+    .then(todo => {
+      if (!user) {
+        return res.status(404).send({
+          message: 'Invalid username or password',
+        });
+      }
+      return res.status(200).send(todo);
+    })
+    .catch(error => res.status(400).send(error));
+},
 };
