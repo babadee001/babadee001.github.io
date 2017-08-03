aaef5411b8881399cdc8c6697cf87c78615f6fd7
babadee001 <temidayooyedele300@gmail.com>
2017-08-03 16:58:30 +0100

    update server file signup routing

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log('Server here');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./routes')(app);
+app.get('*', (req, res) => res.status(404).send({
+  message: 'Nothing to see here',
+}));
+
+module.exports = app;
