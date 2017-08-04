cbafd46165520f455965a53ad57dc826b1049c66
babadee001 <temidayooyedele300@gmail.com>
2017-08-04 15:55:22 +0100

    add easy routing feature for book api

---
+++ server/migrations/20170804104842-create-book-item.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('BookItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      complete: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      bookId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Books',
+          key: 'id',
+          as: 'bookId',
+        },
+      },
+    }),
+  down: (queryInterface, Sequelize) =>
+    queryInterface.dropTable('BookItems'),
+};
